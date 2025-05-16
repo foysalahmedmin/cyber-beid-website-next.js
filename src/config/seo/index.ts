27a0bf786f400f13ac8449fe39056757aa66f89a
foysalahmedmin <foysalahmedmin@gmail.com>
2025-05-17 00:00:08 +0600

    primary setup

---
+++ src/config/seo/index.ts
@@ -0,0 +1,6 @@
+export const SEO = {
+  title: "CyberDoc",
+  description: "Digital solutions.",
+  keywords: ["ui", "ux", "web", "security", "cyber", "digital", "system", "solution"],
+  image: "/images/logo.png",
+};
